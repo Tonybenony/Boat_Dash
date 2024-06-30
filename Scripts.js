5a1998493f59b9134743e1042a7e1321c654b329
Tonybenony <68955840+Tonybenony@users.noreply.github.com>
2024-06-30 22:31:23 +0200

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,8 @@
+function ShowSpeed(){document.getElementById("speed").style.display ="block"}
+function HideSpeed(){document.getElementById("speed").style.display ="none"}
+
+function ShowGps(){document.getElementById("gps").style.display ="block"}
+function HideGps(){document.getElementById("gps").style.display ="none"}
+
+function ShowHeading(){document.getElementById("heading").style.display ="block"}
+function HideHeading(){document.getElementById("heading").style.display ="none"}
