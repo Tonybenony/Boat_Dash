447de0dc54d1aa987ed94f08f22eb4da0291b8c8
Tonybenony <68955840+Tonybenony@users.noreply.github.com>
2024-09-07 00:02:31 +0200

    Add files via upload

--- Arduino/input-device/input-device.ino
+++ Arduino/input-device/input-device.ino
@@ -1,11 +1,17 @@
-int RPMPin = A3;
+int RPMPin = A0;
 int RPMVal = 0; 
 
-int TempPin = A2;
+int TempPin = A1;
 int TempVal = 0; 
 
-int BattPin = A1;
+int BattPin = A2;
 int BattVal = 0; 
+
+int OilPin = A3;
+int OilVal = 0; 
+
+int PressPin = A4;
+int PressVal = 0; 
 
 void setup() {
   Serial.begin(9600);
@@ -15,13 +21,18 @@ void loop() {
 RPMVal = analogRead(RPMPin);
 TempVal = analogRead(TempPin);
 BattVal = analogRead(BattPin);
+OilVal = analogRead(OilPin);
+PressVal = analogRead(PressPin);
 
 Serial.print(RPMVal);Serial.print(";");
 
 Serial.print(TempVal);Serial.print(";");
 
-Serial.print(BattVal);Serial.println(";");
+Serial.print(BattVal);Serial.print(";");
 
+Serial.print(OilVal);Serial.print(";");
+
+Serial.print(PressVal);Serial.println(";");
 
 delay(50);
 }
